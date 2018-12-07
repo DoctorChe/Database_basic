8444f7706a2d44e3e41a06d77d7197d2c699c115
DoctorChe <duncan.reg@yandex.ru>
2018-12-07 09:11:51 +0300

    delete files from main directory

--- explain.sql
+++
@@ -1,3 +0,0 @@
-EXPLAIN `employees`;
-EXPLAIN SELECT * FROM `employees` WHERE `salary` = (SELECT MIN(`salary`) FROM `employees`);
-EXPLAIN SELECT COUNT(`id`) FROM `employees` WHERE `department` = 1;
